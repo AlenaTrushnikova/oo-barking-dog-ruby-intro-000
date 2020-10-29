4bc413a852eaa6abf19fd613bedf7f2478446a11
Alena Trushnikova <alena.yu.t@gmail.com>
2020-10-29 23:12:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,13 @@
 class Dog
   def name
     @name
-end
+  end
 
-def name(dog_name)
-  @name = dog_name
-end
+  def name(dog_name)
+    @name = dog_name
+  end
 
-def bark
-  puts "woof!"
+  def bark
+    puts "woof!"
+  end
 end
